596adb31f7534bc20d625eceb94304b5f362e198
pierre villard <pvillard@ilki.fr>
2021-07-29 14:46:55 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,10 +5,10 @@
  *
  */
 
-$host       = "localhost";
-$username   = "root";
-$password   = "rootroot";
-$dbname     = "test";
+$host       = getenv('BDD_HOST');
+$username   = getenv('BDD_USERNAME');
+$password   = getenv('BDD_PASSWORD');
+$dbname     = getenv('BDD_DATABASE');
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
